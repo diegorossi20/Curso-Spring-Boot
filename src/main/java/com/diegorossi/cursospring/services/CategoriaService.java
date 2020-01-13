05d37964bb833ee7e3817088072ba091fe55b7ea
Diego Rossi <diegorossi20@gmail.com>
2020-01-13 16:46:29 -0300

    Criando Repository e Service para Categoria

---
+++ src/main/java/com/diegorossi/cursospring/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.diegorossi.cursospring.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.diegorossi.cursospring.domain.Categoria;
+import com.diegorossi.cursospring.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
