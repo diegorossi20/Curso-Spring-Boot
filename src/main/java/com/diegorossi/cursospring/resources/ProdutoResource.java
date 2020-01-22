df5c7255b1938cda57339f17303afb1a301881e8
Diego Rossi <diegorossi20@gmail.com>
2020-01-22 11:01:56 -0300

    Atualizando Categoria com PUT e pequenos ajustes

--- src/main/java/com/diegorossi/cursospring/resources/ProdutoResource.java
+++ src/main/java/com/diegorossi/cursospring/resources/ProdutoResource.java
@@ -18,8 +18,8 @@ public class ProdutoResource {
 	private ProdutoService produtoService;
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Produto obj = produtoService.buscar(id);
+	public ResponseEntity<Produto> find(@PathVariable Integer id) {
+		Produto obj = produtoService.find(id);
 		return ResponseEntity.ok().body(obj);
 
 	}
