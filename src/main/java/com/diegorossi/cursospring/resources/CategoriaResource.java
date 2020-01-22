c174f6f9dcc4aba154c8fd1a65b1eaaa81993ae2
Diego Rossi <diegorossi20@gmail.com>
2020-01-22 16:28:14 -0300

    Listando todas categorias com DTO

--- src/main/java/com/diegorossi/cursospring/resources/CategoriaResource.java
+++ src/main/java/com/diegorossi/cursospring/resources/CategoriaResource.java
@@ -1,6 +1,9 @@
 package com.diegorossi.cursospring.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.diegorossi.cursospring.domain.Categoria;
+import com.diegorossi.cursospring.dto.CategoriaDTO;
 import com.diegorossi.cursospring.services.CategoriaService;
 
 @RestController
@@ -40,11 +44,18 @@ public class CategoriaResource {
 		obj = categoriaService.update(obj);
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		categoriaService.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = categoriaService.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
--- src/main/java/com/diegorossi/cursospring/services/CategoriaService.java
+++ src/main/java/com/diegorossi/cursospring/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.diegorossi.cursospring.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,7 +33,7 @@ public class CategoriaService {
 		find(obj.getId());
 		return categoriaRepository.save(obj);
 	}
-	
+
 	public void delete(Integer id) {
 		find(id);
 		try {
@@ -40,6 +41,9 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria com produtos");
 		}
-		
+	}
+
+	public List<Categoria> findAll() {
+		return categoriaRepository.findAll();
 	}
 }
